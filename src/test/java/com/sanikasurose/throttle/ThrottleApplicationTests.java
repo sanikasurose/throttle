c51c7d05c42c7d5ffa191f5617e9606bb6d892f2
Sanika Surose <109695485+sanikasurose@users.noreply.github.com>
2026-03-22 15:58:50 -0400

    Set up project

---
+++ src/test/java/com/sanikasurose/throttle/ThrottleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanikasurose.throttle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrottleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
